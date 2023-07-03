5761a5d5a17fecae5e21f2cf0c5f8eb8d7bd5741
gultar <sacha-olivier.dulac@hotmail.com>
2023-07-03 13:37:17 -0600

    static path is buggy

--- server.py
+++ server.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     create_routes(app)
     app.jinja_env.auto_reload = True
     app.config['TEMPLATES_AUTO_RELOAD'] = True
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port='5000')
     
     
 
