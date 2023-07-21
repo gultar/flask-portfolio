fa37b1072cbee75ceaf5b1f8e57a078d417bbd34
gultar <sacha-olivier.dulac@hotmail.com>
2023-07-21 10:05:40 -0600

    removed terminal page

--- modules/routes/terminal.py
+++
@@ -1,9 +0,0 @@
-# from . import routes_blueprint
-from modules.dependencies import *
-from modules.file_utils import *
-
-# @routes_blueprint.record
-def register_route(routes_blueprint, app, env):
-    @app.route('/terminal', methods=['GET'])
-    def terminal():
-        return render_template('terminal.html')
